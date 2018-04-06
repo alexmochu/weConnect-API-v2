82ec8dbaacfd4735117b95578789af1919bee33a
Mochualex4 <Mochualex4@gmail.com>
2018-04-06 16:48:20 +0300

    add a temporary welcome route

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,7 @@
 # api/__init__.py
 
 # third-party imports
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 # local imports
@@ -14,6 +14,13 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+        # temporary route
+    @app.route('/')
+    def hello_world():
+        response = jsonify({"message": "Welcome to weConnect_V2"})
+        return response
 
     return app
