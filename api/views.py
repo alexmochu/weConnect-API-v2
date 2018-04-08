64af286e37fd5d4790998d406b6484aa85ab13d5
Mochualex4 <Mochualex4@gmail.com>
2018-04-08 21:43:14 +0300

    create token to authenticate and authorize. create signup route and method and create login route and method

--- api/views.py
+++ api/views.py
@@ -4,11 +4,12 @@ import datetime
 import jwt
 
 # universal imports
-from flask import jsonify, request, session
+from flask import Flask, jsonify, request, make_response
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-
-# local imports
+from flask_migrate import Migrate
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # initialize the api
 app = FlaskAPI(__name__, instance_relative_config=True)
@@ -20,8 +21,77 @@ db = SQLAlchemy()
 
 db.init_app(app)
 
+migrate = Migrate(app, db)
+
+from api.models import User
+
+# authorize and authenticate with jwt token
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'header-access-token' in request.headers:
+            token = request.headers['header-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'})
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(username=data['username']).first()
+
+        except: 
+            return jsonify({'message': 'Token is inavlid!'})
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
 # home route
 @app.route('/')
 def hello_world():
     response = jsonify({"message": "Welcome to weConnect_V2"})
+    return response
+
+# create user account
+@app.route('/api/v2/auth/register', methods=['POST'])
+def signup():
+
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'Successfully created an account. Login to access account '})
+
+@app.route('/api/v2/auth/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(username=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'username': user.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+    
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+# user temporary dashboard route
+@app.route('/token_ok')
+@token_required
+def token_ok(current_user):
+    if not current_user:
+        return jsonify({'message': 'Cannot perform that function! Please login'})
+
+    response = jsonify({"message": "Yeaah!! You have successfully authenticated your token"})
     return response
