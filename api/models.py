0e9d3f31a08538659c39e073732320869b878e3e
Mochualex4 <Mochualex4@gmail.com>
2018-05-17 14:09:30 +0300

    add reset password and blacklist token

--- api/models.py
+++ api/models.py
@@ -1,4 +1,9 @@
 # api/models.py
+import jwt
+from datetime import datetime, timedelta
+
+from flask import current_app
+from flask_bcrypt import Bcrypt
 
 from api import db
 
@@ -21,7 +26,62 @@ class User(db.Model):
 
         self.username = username
         self.email = email
-        self.password = password
+        self.password = Bcrypt().generate_password_hash(password).decode()
+
+    
+    def password_is_valid(self, password):
+        """
+        Checks the password against it's hash to validates the user's password
+        """
+
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def save(self):
+        """
+        Save a user to the databse
+        """
+
+        db.session.add(self)
+        db.session.commit()
+
+    
+    def generate_token(self, user_id):
+        """ Generates the access token"""
+
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=60),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS512'
+            )
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+
+    @staticmethod
+    def decode_token(token):
+        """Decodes the access token from the Authorization header."""
+        try:
+            # try to decode the token using our SECRET variable
+            payload = jwt.decode(token, current_app.config.get('SECRET_KEY'))
+            return payload['username']
+        except jwt.ExpiredSignatureError:
+            # the token is expired, return an error string
+            response = {"Expired token. Please login to get a new token"}
+            return response
+        except jwt.InvalidTokenError:
+            # the token is invalid, return an error string
+            response = {"Invalid token. Please register or login"}
+            return response
 
 class Category(db.Model):
     """
@@ -32,7 +92,6 @@ class Category(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(50))
-    description = db.Column(db.String(120))
     created_by = db.Column(db.String, db.ForeignKey('users.username'))
 
     def save(self):
@@ -40,6 +99,13 @@ class Category(db.Model):
         Save a user to the databse
         """
         db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        """
+        Deletes a given category
+        """
+        db.session.delete(self)
         db.session.commit()
 
     def __repr__(self):
@@ -57,15 +123,22 @@ class Business(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     business = db.Column(db.String(50))
-    biz_description = db.Column(db.String(120))
+    business_location = db.Column(db.String(50))
     owner = db.Column(db.String, db.ForeignKey('users.username'))
-    business_categ_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    business_category_id = db.Column(db.Integer, db.ForeignKey('categories.id', ondelete='CASCADE', onupdate='CASCADE'))
 
     def save(self):
         """
         Save a bsuiness to the databse
         """
         db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """
+        Deletes a given category
+        """
+        db.session.delete(self)
         db.session.commit()
 
 
@@ -85,7 +158,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     review = db.Column(db.String(600))
     reviewer = db.Column(db.String(50), db.ForeignKey('users.username'))
-    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'))
+    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id', ondelete='CASCADE', onupdate='CASCADE'))
 
     def save(self):
         """
@@ -101,3 +174,22 @@ class Review(db.Model):
         """
         return "<Review: {}>".format(self.review)
    
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing blacklisted JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+
+    def save(self):
+        """Save token"""
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
