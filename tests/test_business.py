e2cfbe97b09364a025ef99f66fb0783cb8f025e2
Mochualex4 <Mochualex4@gmail.com>
2018-05-17 18:29:52 +0300

    updayte travis

--- tests/test_business.py
+++ tests/test_business.py
@@ -14,7 +14,7 @@ class BusinessTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
-        self.app.config.update(SQLALCHEMY_DATABASE_URI='postgresql://postgres:mypassword@localhost/weConnect_test')
+        self.app.config.update(SQLALCHEMY_DATABASE_URI='postgresql://localhost/weConnect_test')
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.client = self.app.test_client
