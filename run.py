728c008ea4a5548e65c93ca23a9cc5219ec5b96c
Mochualex4 <Mochualex4@gmail.com>
2018-05-20 18:03:43 +0300

    update run.py

--- run.py
+++ run.py
@@ -8,4 +8,5 @@ config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 8000))
+    app.run('', port=port)
