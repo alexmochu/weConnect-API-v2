0386f46da8f8d56aae9acd69a7b731cc75b4fc2a
Mochualex4 <Mochualex4@gmail.com>
2018-05-21 07:11:19 +0300

    update

--- api/__init__.py
+++ api/__init__.py
@@ -13,7 +13,7 @@ from config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     #app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
